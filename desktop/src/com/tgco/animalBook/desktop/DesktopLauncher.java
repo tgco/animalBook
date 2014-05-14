89079ab36e4efb32c66fdb776408ebc662480cfb
Walter Schlosser <wschloss@mymail.mines.edu>
2014-05-14 14:40:30 -0600

    Initial commit with project template

---
+++ desktop/src/com/tgco/animalBook/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tgco.animalBook.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tgco.animalBook.AnimalBookGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AnimalBookGame(), config);
+	}
+}

7acb09bbdcf8b0334d9e46dc761b794c1bec1571
Walter Schlosser <wschloss@mymail.mines.edu>
2014-05-16 11:50:41 -0600

    Added a texture atlas to the adapter for use with packaged textures for each button

--- core/src/com/tgco/animalBook/screens/ButtonScreenAdapter.java
+++ core/src/com/tgco/animalBook/screens/ButtonScreenAdapter.java
@@ -2,6 +2,7 @@ package com.tgco.animalBook.screens;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.tgco.animalBook.AnimalBookGame;
@@ -12,6 +13,9 @@ public abstract class ButtonScreenAdapter {
 	private Skin buttonSkin;
 	private Stage buttonStage;
 	private AnimalBookGame gameInstance;
+	
+	//Button texture initialization
+	private TextureAtlas atlas;
 
 	public ButtonScreenAdapter(AnimalBookGame gameInstance) {
 		this.gameInstance = gameInstance;

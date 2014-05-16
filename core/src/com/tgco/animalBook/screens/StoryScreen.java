145af94851561ce2dc5488bcad514aba3d355c26
aaflack18 <kmasuda@mines.edu>
2014-05-16 12:49:00 -0600

    Created StoryScreen and LoseScreen
    Just created them. Does nothing. LOL.

---
+++ core/src/com/tgco/animalBook/screens/StoryScreen.java
@@ -0,0 +1,55 @@
+package com.tgco.animalBook.screens;
+
+import com.badlogic.gdx.Screen;
+import com.tgco.animalBook.AnimalBookGame;
+
+public class StoryScreen extends ButtonScreenAdapter implements Screen {
+
+	public StoryScreen(AnimalBookGame gameInstance) {
+		super(gameInstance);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

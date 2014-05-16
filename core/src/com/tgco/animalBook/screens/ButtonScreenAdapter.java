cfd300524b87d471104fa22bf74a1c9d36186713
Daniel Victor <dvictor@Mines.edu>
2014-05-16 13:03:01 -0600

    added textures for buttons on options screen and inventory screen

--- core/src/com/tgco/animalBook/screens/ButtonScreenAdapter.java
+++ core/src/com/tgco/animalBook/screens/ButtonScreenAdapter.java
@@ -2,7 +2,6 @@ package com.tgco.animalBook.screens;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.tgco.animalBook.AnimalBookGame;
@@ -13,9 +12,6 @@ public abstract class ButtonScreenAdapter {
 	private Skin buttonSkin;
 	private Stage buttonStage;
 	private AnimalBookGame gameInstance;
-	
-	//Button texture initialization
-	private TextureAtlas atlas;
 
 	public ButtonScreenAdapter(AnimalBookGame gameInstance) {
 		this.gameInstance = gameInstance;

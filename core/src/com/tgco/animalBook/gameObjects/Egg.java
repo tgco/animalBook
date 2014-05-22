09d9f168e9a63d19022fbbb0e101cf48fafbc45a
Kelly Masuda <kmasuda@mines.edu>
2014-05-21 23:38:38 -0600

    added attemptHatching() method to Egg
    Because who's gonna get me my scrambled eggs?
    
    Method will return true if the hatching is successful.

--- core/src/com/tgco/animalBook/gameObjects/Egg.java
+++ core/src/com/tgco/animalBook/gameObjects/Egg.java
@@ -1,8 +1,11 @@
 package com.tgco.animalBook.gameObjects;
 
+import java.util.Random;
+
 import com.badlogic.gdx.math.Vector2;
 
 public class Egg extends Consumable {
+	private static final Random rand = new Random();
 
 	public Egg(String texturePath) {
 		super(texturePath);
@@ -12,6 +15,12 @@ public class Egg extends Consumable {
 	public Egg(String texturePath, Vector2 position) {
 		super(texturePath, position);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Boolean attemptHatching(){	
+		if (rand.nextInt(10000) == 0)
+			return true;
+		return false;
 	}
 
 }

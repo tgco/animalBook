56d7bff23669e6b86393a9dc2ca602027e66cf29
Kelly Masuda <kmasuda@mines.edu>
2014-05-22 11:40:16 -0600

    undid inventory

--- core/src/com/tgco/animalBook/gameObjects/Inventory.java
+++ core/src/com/tgco/animalBook/gameObjects/Inventory.java
@@ -1,16 +1,15 @@
 package com.tgco.animalBook.gameObjects;
 
+import java.util.ArrayList;
 import java.util.TreeMap;
-
-import com.badlogic.gdx.utils.Array;
 
 public class Inventory {
 
-	private static TreeMap<Consumable.DropType, Array<Consumable>> inventory;
+	private static TreeMap<Consumable.DropType, ArrayList<Consumable>> inventory;
 	public Inventory() {
 		Consumable.DropType[] dropTypes = Consumable.DropType.values();
 		for (int i = 0; i < dropTypes.length; i++){
-			inventory.put(dropTypes[i], new Array<Consumable>());
+			inventory.put(dropTypes[i], new ArrayList<Consumable>());
 		}
 	}
 

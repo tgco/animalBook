9d272c99ad906588f35c3ea9040386b7333ab655
Walter Schlosser <wschloss@mymail.mines.edu>
2014-05-28 13:51:56 -0600

    Javadoc added to animal subcalsses

--- core/src/com/tgco/animalBook/gameObjects/Goose.java
+++ core/src/com/tgco/animalBook/gameObjects/Goose.java
@@ -8,23 +8,39 @@ import com.tgco.animalBook.gameObjects.Consumable.DropType;
 
 public class Goose extends Animal {
 
+	/**
+	 * The type of item this animal drops
+	 */
 	private static final DropType dropType = Consumable.DropType.EGG;
 	
+	/**
+	 * Path to the texture for this animal
+	 */
 	private static final String texturePath = "objectTextures/goose.png";
 	
+	/**
+	 * Constructor that takes a position
+	 * 
+	 * @param pos the desired position in world coordinates for this animal
+	 */
 	public Goose(Vector2 pos) {
 		super(texturePath, pos);
-		
 		
 		if (AnimalBookGame.tapControls)
 			speed = 1/2f;
 		else
 			speed = 1/10f;
+		
 		width = .093f*Gdx.graphics.getWidth();
 		height = .147f*Gdx.graphics.getHeight();
 		bounds = new Rectangle(position.x - width/2,position.y - height/2,width,height);
 	}
 	
+	/**
+	 * Returns the type of item this animal drops
+	 * 
+	 * @return		the drop type for this animal
+	 */
 	@Override
 	public DropType getDropType() {
 		return dropType;

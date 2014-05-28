a034701e4bdbe91ed330ae0e68079037b0086278
Walter Schlosser <wschloss@mymail.mines.edu>
2014-05-28 13:37:44 -0600

    Added javadoc to gameScreen, gameScreenInputHandler, and world

--- core/src/com/tgco/animalBook/screens/GameScreen.java
+++ core/src/com/tgco/animalBook/screens/GameScreen.java
@@ -25,35 +25,60 @@ import com.tgco.animalBook.view.World;
 
 public class GameScreen extends ButtonScreenAdapter implements Screen {
 
-	//World of objects
+	/**
+	 * Reference to the game world where all objects are located
+	 */
 	private World gameWorld;
 
-	//buttons
+	/**
+	 * Each button used on the game screen user interface overlay
+	 */
 	private Button inventoryButton;
 	private Button upgradeButton;
 	private Button pauseButton;
 	private Button eatButton;
+
+	/**
+	 * Reference to the upgrades screen to pass along values about the game world
+	 */
 	private UpgradesScreen upgradeScreen;
-	
+
+	/**
+	 * Stage to draw the screen once the player has lost
+	 */
 	private Stage popupStage;
+
+	/**
+	 * Determines if the player has lost
+	 */
 	private boolean hasLost = false;
-	
-	
-	
+
+	/**
+	 * The font used when rendering strings
+	 */
 	private BitmapFont font;
-	
-	
+
+	/**
+	 * Determines if the game is paused
+	 */
 	boolean paused;
 
+	/**
+	 * Constructor using the running game instance
+	 * 
+	 * @param gameInstance a reference to the currently running game instance
+	 */
 	public GameScreen(AnimalBookGame gameInstance) {
 		super(gameInstance);
+
 		popupStage = new Stage();
 		paused = false;
 
 		//Initialize game world
 		gameWorld = new World(gameInstance);
-		font = new BitmapFont(Gdx.files.internal("fonts/SketchBook.fnt"));
+
 		//Initialize rendering objects
+		font = new BitmapFont(Gdx.files.internal("fonts/SketchBook.fnt"));
 		batch = new SpriteBatch();
 		batch.setProjectionMatrix(gameWorld.getCamera().combined);
 		backgroundTexture = new Texture(Gdx.files.internal("backgrounds/gameScreenGrass2.jpg"));
@@ -64,66 +89,73 @@ public class GameScreen extends ButtonScreenAdapter implements Screen {
 		inputMultiplexer.addProcessor(touchControls);
 		Gdx.input.setInputProcessor(inputMultiplexer);
 		Gdx.input.setCatchBackKey(true);
-		
-		
 	}
 
+	/**
+	 * Draws the buttons and background as well as handles button presses
+	 * 
+	 * @param delta the time between two frames
+	 */
 	@Override
 	public void render(float delta) {
-		
-		if(!hasLost ){
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		//Process button presses
-		buttonStage.act(delta);
+		if(!hasLost) {
+			Gdx.gl.glClearColor(0, 0, 0, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		//render background and world
-		batch.setProjectionMatrix(gameWorld.getCamera().combined);
+			//Process button presses
+			buttonStage.act(delta);
 
-		//Find the node on screen to draw grass around
-		Vector2 tileNode = findTileNodeOnScreen();
+			//render background and world
+			batch.setProjectionMatrix(gameWorld.getCamera().combined);
 
-		batch.begin();
+			//Find the node on screen to draw grass around
+			Vector2 tileNode = findTileNodeOnScreen();
 
-		//Draw four grass textures around the node on screen
-		batch.draw(backgroundTexture, tileNode.x*Gdx.graphics.getWidth(), tileNode.y*Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(backgroundTexture, (tileNode.x-1)*Gdx.graphics.getWidth(), tileNode.y*Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(backgroundTexture, tileNode.x*Gdx.graphics.getWidth(), (tileNode.y-1)*Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(backgroundTexture, (tileNode.x-1)*Gdx.graphics.getWidth(), (tileNode.y-1)*Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		
-		//Draw world over background
-		font.setColor(Color.BLACK);
-		font.setScale(1.2f);
-		Vector3 vect = new Vector3(Gdx.graphics.getWidth()/2 +10,0 +3*EDGE_TOLERANCE,0);
-		gameWorld.getCamera().unproject(vect);
-		font.draw(batch, "Your Money: $" + String.valueOf(gameWorld.getPlayer().getPlayerMoney()), vect.x ,vect.y );
+			batch.begin();
 
-		gameWorld.render(batch,paused);
-		
-		
-		batch.end();
+			//Draw four grass textures around the node on screen
+			batch.draw(backgroundTexture, tileNode.x*Gdx.graphics.getWidth(), tileNode.y*Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			batch.draw(backgroundTexture, (tileNode.x-1)*Gdx.graphics.getWidth(), tileNode.y*Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			batch.draw(backgroundTexture, tileNode.x*Gdx.graphics.getWidth(), (tileNode.y-1)*Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			batch.draw(backgroundTexture, (tileNode.x-1)*Gdx.graphics.getWidth(), (tileNode.y-1)*Gdx.graphics.getHeight(), Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-		//Draw buttons over the screen
-		buttonStage.draw();
-		
+			//Draw players current money
+			font.setColor(Color.BLACK);
+			font.setScale(1.2f);
+			Vector3 vect = new Vector3(Gdx.graphics.getWidth()/2 +10,0 +3*EDGE_TOLERANCE,0);
+			gameWorld.getCamera().unproject(vect);
+			font.draw(batch, "Your Money: $" + String.valueOf(gameWorld.getPlayer().getPlayerMoney()), vect.x ,vect.y );
+
+			//Draw world over background
+			gameWorld.render(batch,paused);
+			
+			batch.end();
+
+			//Draw buttons over the screen
+			buttonStage.draw();
+
 		}
-		else{
+		else { //if player lost
 			Gdx.gl.glClearColor(1, 1, 1, 1);
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-			
+
 			popupStage.act(delta);
 			popupStage.draw();
 		}
 	}
 
-	//Finds which "node" is visible on screen and draws four grass tiles around it
+	/**
+	 * Returns the point in world coordinates that grass should be drawn around so that grass
+	 * is only rendered when it is on screen.
+	 * 
+	 * @return		the point on screen to draw grass around
+	 */
 	public Vector2 findTileNodeOnScreen() {
 
 		//If camera is in the negative quadrant
 		boolean flipX = false;
 		boolean flipY = false;
-
 		if (gameWorld.getCamera().position.x < 0)
 			flipX = true;
 		if (gameWorld.getCamera().position.y < 0)
@@ -141,6 +173,12 @@ public class GameScreen extends ButtonScreenAdapter implements Screen {
 		return new Vector2(xCoordinate,yCoordinate);
 	}
 
+	/**
+	 * Runs when the screen is resized in order to keep button sizes and locations correct
+	 * 
+	 * @param width the new width of the screen
+	 * @param height the new height of the screen
+	 */
 	@Override
 	public void resize(int width, int height) {
 		if ( buttonStage == null)
@@ -148,10 +186,11 @@ public class GameScreen extends ButtonScreenAdapter implements Screen {
 		buttonStage.clear();
 		//reinit buttons
 		initializeButtons();
-		
-		
 	}
 
+	/**
+	 * Initializes all button objects
+	 */
 	@Override
 	protected void initializeButtons() {
 
@@ -184,13 +223,13 @@ public class GameScreen extends ButtonScreenAdapter implements Screen {
 		inventoryButton.setHeight(BUTTON_HEIGHT);
 		inventoryButton.setX(EDGE_TOLERANCE);
 		inventoryButton.setY(Gdx.graphics.getHeight() - 2*BUTTON_HEIGHT - EDGE_TOLERANCE);
-		
+
 		//EAT BUTTON
 		//TEMP FOR TESTING
 		atlas = new TextureAtlas(Gdx.files.internal("buttons/gameScreen/eatButton.atlas"));
 		buttonSkin = new Skin();
 		buttonSkin.addRegions(atlas);
-		
+
 		ButtonStyle eatButtonStyle = new ButtonStyle();
 		eatButtonStyle.up = buttonSkin.getDrawable("buttonUnpressed");
 		eatButtonStyle.down = buttonSkin.getDrawable("buttonPressed");
@@ -245,7 +284,7 @@ public class GameScreen extends ButtonScreenAdapter implements Screen {
 				gameInstance.setScreen(new InventoryScreen(gameInstance,GameScreen.this));
 			}
 		});
-		
+
 		eatButton.addListener(new InputListener() {
 			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
 				return true;
@@ -270,6 +309,7 @@ public class GameScreen extends ButtonScreenAdapter implements Screen {
 			}
 		});
 
+		//add to stage for input detection
 		buttonStage.addActor(inventoryButton);
 		buttonStage.addActor(upgradeButton);
 		buttonStage.addActor(pauseButton);
@@ -278,9 +318,60 @@ public class GameScreen extends ButtonScreenAdapter implements Screen {
 		inputMultiplexer.addProcessor(buttonStage);
 	}
 
+	/**
+	 * Disposes of all objects that should release memory
+	 */
+	@Override
+	public void dispose() {
+		super.dispose();
+		gameWorld.dispose();
+	}
+
+	/**
+	 * Resets the input processor controls to the input setup for the game screen
+	 */
+	public void resetInputProcessors() {
+		Gdx.input.setInputProcessor(inputMultiplexer);
+	}
+
+	/**
+	 * Sets the game to the lost mode when the player has lost
+	 * 
+	 * @param noAnimals true if the player lost due to losing all animals
+	 */
+	public void setLost(boolean noAnimals){
+		hasLost = true;
+		Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+		LostDialog lostD = new LostDialog("You Lost", skin, gameInstance,noAnimals);
+		lostD.show(popupStage);
+		popupStage.addActor(lostD);
+		inputMultiplexer.addProcessor(popupStage);
+	}
+	
+	/**
+	 * Returns the instance of the world that game screen is using
+	 * 
+	 * @return		reference to the current game world
+	 */
+	public World getWorld() {
+		return gameWorld;
+	}
+
+	/**
+	 * Returns if the game is currently paused
+	 * 
+	 * @return		true if the game is paused
+	 */
+	public boolean isPaused() {
+		return paused;
+	}
+	
+	/**
+	 * Unused methods for detecting screen events
+	 */
 	@Override
 	public void show() {
-		
+
 	}
 
 	@Override
@@ -299,33 +390,6 @@ public class GameScreen extends ButtonScreenAdapter implements Screen {
 	public void resume() {
 		// TODO Auto-generated method stub
 
-	}
-
-	@Override
-	public void dispose() {
-		super.dispose();
-		gameWorld.dispose();
-	}
-
-	public World getWorld() {
-		return gameWorld;
-	}
-	
-	public boolean isPaused() {
-		return paused;
-	}
-
-	public void resetInputProcessors() {
-		Gdx.input.setInputProcessor(inputMultiplexer);
-	}
-	
-	public void setLost(boolean noAnimals){
-		hasLost = true;
-		Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
-		lostDialog lostD = new lostDialog("You Lost", skin, gameInstance,noAnimals);
-		lostD.show(popupStage);
-		popupStage.addActor(lostD);
-		inputMultiplexer.addProcessor(popupStage);
 	}
 
 }
--- core/src/com/tgco/animalBook/screens/LostDialog.java
+++ core/src/com/tgco/animalBook/screens/LostDialog.java
@@ -5,10 +5,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.tgco.animalBook.AnimalBookGame;
 import com.tgco.animalBook.handlers.SoundHandler;
 
-public class lostDialog extends Dialog{
+public class LostDialog extends Dialog{
 
 	private AnimalBookGame gameInstance;
-	public lostDialog(String title, Skin skin, AnimalBookGame gameInstance, boolean noAnimals) {
+	public LostDialog(String title, Skin skin, AnimalBookGame gameInstance, boolean noAnimals) {
 		super(title, skin);
 		this.gameInstance = gameInstance;
 		if(noAnimals){

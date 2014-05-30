008f68d220bc415b69901523eb8de814e5ed5f47
Kelly Masuda <kmasuda@mines.edu>
2014-05-30 12:05:11 -0600

    COmmitted to my inventory

--- core/src/com/tgco/animalBook/gameObjects/Obstacle.java
+++ core/src/com/tgco/animalBook/gameObjects/Obstacle.java
@@ -1,5 +1,18 @@
 package com.tgco.animalBook.gameObjects;
 
+import java.util.Random;
+
+import com.badlogic.gdx.math.Rectangle;
+
 public class Obstacle extends ABDrawable {
+	
+	private static final String texturePath = "objectTextures/player.png";
+	private static final Random rand = new Random();
+
+	public Obstacle() {
+		super(texturePath);
+		
+		
+	}
 
 }

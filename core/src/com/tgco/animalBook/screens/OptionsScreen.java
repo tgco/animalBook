d2076a8afd828cf18f7531ba3a858403a0d7b003
Daniel Victor <dvictor@Mines.edu>
2014-06-12 13:29:45 -0600

    more font changes

--- core/src/com/tgco/animalBook/screens/OptionsScreen.java
+++ core/src/com/tgco/animalBook/screens/OptionsScreen.java
@@ -50,7 +50,7 @@ public class OptionsScreen extends ButtonScreenAdapter implements Screen {
 		
 		//Initialize rendering objects
 		font = new BitmapFont(Gdx.files.internal("fonts/Dimbo2.fnt"));
-		font.setScale(.75f);
+		font.setScale(Gdx.graphics.getHeight()/750f);
 
 		//Background Rendering
 		batch = new SpriteBatch();
@@ -353,7 +353,7 @@ public class OptionsScreen extends ButtonScreenAdapter implements Screen {
 	public void setDialog(){
 		hasConfirm  = true;
 		Skin skin = new Skin(Gdx.files.internal("confirmSkin/uiskin.json"));
-		ConfirmDialog resetD = new ConfirmDialog("RESET DATA", skin, gameInstance,"All progress will be deleted. Are you Sure you want to reset?", 2);
+		ConfirmDialog resetD = new ConfirmDialog("RESET DATA", skin, gameInstance,"All progress will be lost. Are you sure you want to reset?", 2);
 		resetD.show(popupStage);
 		popupStage.addActor(resetD);
 		inputMultiplexer.addProcessor(popupStage);

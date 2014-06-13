171a3339b167d2d0b818de9f704e61727cf2acf9
Kelly Masuda <kmasuda@mines.edu>
2014-06-13 12:18:35 -0600

    implemented windy physics

--- core/src/com/tgco/animalBook/view/WorldRenderer.java
+++ core/src/com/tgco/animalBook/view/WorldRenderer.java
@@ -158,10 +158,8 @@ public class WorldRenderer {
 		projectedBatch.draw(progressBar,Gdx.graphics.getWidth() - 2f*(.019f)*Gdx.graphics.getWidth(), (.029f)*Gdx.graphics.getHeight(), PROGRESS_BAR_WIDTH, PROGRESS_BAR_HEIGHT);
 		projectedBatch.draw(black,Gdx.graphics.getWidth() - 2f*(.019f)*Gdx.graphics.getWidth() + .0025f*Gdx.graphics.getWidth(), (.029f)*Gdx.graphics.getHeight() + progressPercentage*(PROGRESS_BAR_HEIGHT), PROGRESS_SLIDER_WIDTH, PROGRESS_SLIDER_HEIGHT);
 		
-		
 		projectedBatch.end();
 		projectedBatch.dispose();
-
 
 		batch.begin();
 	}
